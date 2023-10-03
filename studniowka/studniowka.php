d3252859d017d48cdbabc0afc9ae058e05d6d530
1dorsh <hubert.pena23@gmail.com>
2023-10-03 10:33:26 +0200

    Data studniowki

---
+++ studniowka/studniowka.php
@@ -0,0 +1,10 @@
+<?php
+    $start_date = date( "06.05.2024");  
+    echo "Data dzisiejsza: ". $start_date . "<br>";
+    $date = strtotime($start_date);
+    $date = strtotime("-100 day", $date);
+    echo "Data studniówki: " . date('d.m.Y', $date);
+?>
+
+
+
